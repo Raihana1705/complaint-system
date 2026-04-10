c87bb2eaf0d77acd1af620793e6768719a3558d9
Raihana1705 <raihanarain2@gmail.com>
2026-04-10 19:55:34 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,101 +1,186 @@
-# Natural Language Toolkit: Classifiers
+# Natural Language Toolkit: Corpus Readers
 #
 # Copyright (C) 2001-2023 NLTK Project
-# Author: Edward Loper <edloper@gmail.com>
+# Author: Steven Bird <stevenbird1@gmail.com>
+#         Edward Loper <edloper@gmail.com>
 # URL: <https://www.nltk.org/>
 # For license information, see LICENSE.TXT
 
 """
-Classes and interfaces for labeling tokens with category labels (or
-"class labels").  Typically, labels are represented with strings
-(such as ``'health'`` or ``'sports'``).  Classifiers can be used to
-perform a wide range of classification tasks.  For example,
-classifiers can be used...
+NLTK corpus readers.  The modules in this package provide functions
+that can be used to read corpus fileids in a variety of formats.  These
+functions can be used to read both the corpus fileids that are
+distributed in the NLTK corpus package, and corpus fileids that are part
+of external corpora.
 
-- to classify documents by topic
-- to classify ambiguous words by which word sense is intended
-- to classify acoustic signals by which phoneme they represent
-- to classify sentences by their author
+Corpus Reader Functions
+=======================
+Each corpus module defines one or more "corpus reader functions",
+which can be used to read documents from that corpus.  These functions
+take an argument, ``item``, which is used to indicate which document
+should be read from the corpus:
 
-Features
-========
-In order to decide which category label is appropriate for a given
-token, classifiers examine one or more 'features' of the token.  These
-"features" are typically chosen by hand, and indicate which aspects
-of the token are relevant to the classification decision.  For
-example, a document classifier might use a separate feature for each
-word, recording how often that word occurred in the document.
+- If ``item`` is one of the unique identifiers listed in the corpus
+  module's ``items`` variable, then the corresponding document will
+  be loaded from the NLTK corpus package.
+- If ``item`` is a fileid, then that file will be read.
 
-Featuresets
-===========
-The features describing a token are encoded using a "featureset",
-which is a dictionary that maps from "feature names" to "feature
-values".  Feature names are unique strings that indicate what aspect
-of the token is encoded by the feature.  Examples include
-``'prevword'``, for a feature whose value is the previous word; and
-``'contains-word(library)'`` for a feature that is true when a document
-contains the word ``'library'``.  Feature values are typically
-booleans, numbers, or strings, depending on which feature they
-describe.
+Additionally, corpus reader functions can be given lists of item
+names; in which case, they will return a concatenation of the
+corresponding documents.
 
-Featuresets are typically constructed using a "feature detector"
-(also known as a "feature extractor").  A feature detector is a
-function that takes a token (and sometimes information about its
-context) as its input, and returns a featureset describing that token.
-For example, the following feature detector converts a document
-(stored as a list of words) to a featureset describing the set of
-words included in the document:
+Corpus reader functions are named based on the type of information
+they return.  Some common examples, and their return types, are:
 
-    >>> # Define a feature detector function.
-    >>> def document_features(document):
-    ...     return dict([('contains-word(%s)' % w, True) for w in document])
+- words(): list of str
+- sents(): list of (list of str)
+- paras(): list of (list of (list of str))
+- tagged_words(): list of (str,str) tuple
+- tagged_sents(): list of (list of (str,str))
+- tagged_paras(): list of (list of (list of (str,str)))
+- chunked_sents(): list of (Tree w/ (str,str) leaves)
+- parsed_sents(): list of (Tree with str leaves)
+- parsed_paras(): list of (list of (Tree with str leaves))
+- xml(): A single xml ElementTree
+- raw(): unprocessed corpus contents
 
-Feature detectors are typically applied to each token before it is fed
-to the classifier:
+For example, to read a list of the words in the Brown Corpus, use
+``nltk.corpus.brown.words()``:
 
-    >>> # Classify each Gutenberg document.
-    >>> from nltk.corpus import gutenberg
-    >>> for fileid in gutenberg.fileids(): # doctest: +SKIP
-    ...     doc = gutenberg.words(fileid) # doctest: +SKIP
-    ...     print(fileid, classifier.classify(document_features(doc))) # doctest: +SKIP
+    >>> from nltk.corpus import brown
+    >>> print(", ".join(brown.words()[:6])) # only first 6 words
+    The, Fulton, County, Grand, Jury, said
 
-The parameters that a feature detector expects will vary, depending on
-the task and the needs of the feature detector.  For example, a
-feature detector for word sense disambiguation (WSD) might take as its
-input a sentence, and the index of a word that should be classified,
-and return a featureset for that word.  The following feature detector
-for WSD includes features describing the left and right contexts of
-the target word:
-
-    >>> def wsd_features(sentence, index):
-    ...     featureset = {}
-    ...     for i in range(max(0, index-3), index):
-    ...         featureset['left-context(%s)' % sentence[i]] = True
-    ...     for i in range(index, max(index+3, len(sentence))):
-    ...         featureset['right-context(%s)' % sentence[i]] = True
-    ...     return featureset
-
-Training Classifiers
-====================
-Most classifiers are built by training them on a list of hand-labeled
-examples, known as the "training set".  Training sets are represented
-as lists of ``(featuredict, label)`` tuples.
+isort:skip_file
 """
 
-from nltk.classify.api import ClassifierI, MultiClassifierI
-from nltk.classify.decisiontree import DecisionTreeClassifier
-from nltk.classify.maxent import (
-    BinaryMaxentFeatureEncoding,
-    ConditionalExponentialClassifier,
-    MaxentClassifier,
-    TypedMaxentFeatureEncoding,
-)
-from nltk.classify.megam import call_megam, config_megam
-from nltk.classify.naivebayes import NaiveBayesClassifier
-from nltk.classify.positivenaivebayes import PositiveNaiveBayesClassifier
-from nltk.classify.rte_classify import RTEFeatureExtractor, rte_classifier, rte_features
-from nltk.classify.scikitlearn import SklearnClassifier
-from nltk.classify.senna import Senna
-from nltk.classify.textcat import TextCat
-from nltk.classify.util import accuracy, apply_features, log_likelihood
-from nltk.classify.weka import WekaClassifier, config_weka
+from nltk.corpus.reader.plaintext import *
+from nltk.corpus.reader.util import *
+from nltk.corpus.reader.api import *
+from nltk.corpus.reader.tagged import *
+from nltk.corpus.reader.cmudict import *
+from nltk.corpus.reader.conll import *
+from nltk.corpus.reader.chunked import *
+from nltk.corpus.reader.wordlist import *
+from nltk.corpus.reader.xmldocs import *
+from nltk.corpus.reader.ppattach import *
+from nltk.corpus.reader.senseval import *
+from nltk.corpus.reader.ieer import *
+from nltk.corpus.reader.sinica_treebank import *
+from nltk.corpus.reader.bracket_parse import *
+from nltk.corpus.reader.indian import *
+from nltk.corpus.reader.toolbox import *
+from nltk.corpus.reader.timit import *
+from nltk.corpus.reader.ycoe import *
+from nltk.corpus.reader.rte import *
+from nltk.corpus.reader.string_category import *
+from nltk.corpus.reader.propbank import *
+from nltk.corpus.reader.verbnet import *
+from nltk.corpus.reader.bnc import *
+from nltk.corpus.reader.nps_chat import *
+from nltk.corpus.reader.wordnet import *
+from nltk.corpus.reader.switchboard import *
+from nltk.corpus.reader.dependency import *
+from nltk.corpus.reader.nombank import *
+from nltk.corpus.reader.ipipan import *
+from nltk.corpus.reader.pl196x import *
+from nltk.corpus.reader.knbc import *
+from nltk.corpus.reader.chasen import *
+from nltk.corpus.reader.childes import *
+from nltk.corpus.reader.aligned import *
+from nltk.corpus.reader.lin import *
+from nltk.corpus.reader.semcor import *
+from nltk.corpus.reader.framenet import *
+from nltk.corpus.reader.udhr import *
+from nltk.corpus.reader.bnc import *
+from nltk.corpus.reader.sentiwordnet import *
+from nltk.corpus.reader.twitter import *
+from nltk.corpus.reader.nkjp import *
+from nltk.corpus.reader.crubadan import *
+from nltk.corpus.reader.mte import *
+from nltk.corpus.reader.reviews import *
+from nltk.corpus.reader.opinion_lexicon import *
+from nltk.corpus.reader.pros_cons import *
+from nltk.corpus.reader.categorized_sents import *
+from nltk.corpus.reader.comparative_sents import *
+from nltk.corpus.reader.panlex_lite import *
+from nltk.corpus.reader.panlex_swadesh import *
+from nltk.corpus.reader.bcp47 import *
+
+# Make sure that nltk.corpus.reader.bracket_parse gives the module, not
+# the function bracket_parse() defined in nltk.tree:
+from nltk.corpus.reader import bracket_parse
+
+__all__ = [
+    "CorpusReader",
+    "CategorizedCorpusReader",
+    "PlaintextCorpusReader",
+    "find_corpus_fileids",
+    "TaggedCorpusReader",
+    "CMUDictCorpusReader",
+    "ConllChunkCorpusReader",
+    "WordListCorpusReader",
+    "PPAttachmentCorpusReader",
+    "SensevalCorpusReader",
+    "IEERCorpusReader",
+    "ChunkedCorpusReader",
+    "SinicaTreebankCorpusReader",
+    "BracketParseCorpusReader",
+    "IndianCorpusReader",
+    "ToolboxCorpusReader",
+    "TimitCorpusReader",
+    "YCOECorpusReader",
+    "MacMorphoCorpusReader",
+    "SyntaxCorpusReader",
+    "AlpinoCorpusReader",
+    "RTECorpusReader",
+    "StringCategoryCorpusReader",
+    "EuroparlCorpusReader",
+    "CategorizedBracketParseCorpusReader",
+    "CategorizedTaggedCorpusReader",
+    "CategorizedPlaintextCorpusReader",
+    "PortugueseCategorizedPlaintextCorpusReader",
+    "tagged_treebank_para_block_reader",
+    "PropbankCorpusReader",
+    "VerbnetCorpusReader",
+    "BNCCorpusReader",
+    "ConllCorpusReader",
+    "XMLCorpusReader",
+    "NPSChatCorpusReader",
+    "SwadeshCorpusReader",
+    "WordNetCorpusReader",
+    "WordNetICCorpusReader",
+    "SwitchboardCorpusReader",
+    "DependencyCorpusReader",
+    "NombankCorpusReader",
+    "IPIPANCorpusReader",
+    "Pl196xCorpusReader",
+    "TEICorpusView",
+    "KNBCorpusReader",
+    "ChasenCorpusReader",
+    "CHILDESCorpusReader",
+    "AlignedCorpusReader",
+    "TimitTaggedCorpusReader",
+    "LinThesaurusCorpusReader",
+    "SemcorCorpusReader",
+    "FramenetCorpusReader",
+    "UdhrCorpusReader",
+    "BNCCorpusReader",
+    "SentiWordNetCorpusReader",
+    "SentiSynset",
+    "TwitterCorpusReader",
+    "NKJPCorpusReader",
+    "CrubadanCorpusReader",
+    "MTECorpusReader",
+    "ReviewsCorpusReader",
+    "OpinionLexiconCorpusReader",
+    "ProsConsCorpusReader",
+    "CategorizedSentencesCorpusReader",
+    "ComparativeSentencesCorpusReader",
+    "PanLexLiteCorpusReader",
+    "NonbreakingPrefixesCorpusReader",
+    "UnicharsCorpusReader",
+    "MWAPPDBCorpusReader",
+    "PanlexSwadeshCorpusReader",
+    "BCP47CorpusReader",
+]

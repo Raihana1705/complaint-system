871c5ff2a9219079ccc826a34a7cb119d337c188
Raihana1705 <raihanarain2@gmail.com>
2026-04-10 19:50:52 +0530

    Add files via upload

--- help.py
+++ help.py
@@ -1,37 +1,64 @@
-from __future__ import absolute_import
+# Natural Language Toolkit (NLTK) Help
+#
+# Copyright (C) 2001-2023 NLTK Project
+# Authors: Steven Bird <stevenbird1@gmail.com>
+# URL: <https://www.nltk.org/>
+# For license information, see LICENSE.TXT
 
-from pip._internal.cli.base_command import Command
-from pip._internal.cli.status_codes import SUCCESS
-from pip._internal.exceptions import CommandError
+"""
+Provide structured access to documentation.
+"""
+
+import re
+from textwrap import wrap
+
+from nltk.data import load
 
 
-class HelpCommand(Command):
-    """Show help for commands"""
-    name = 'help'
-    usage = """
-      %prog <command>"""
-    summary = 'Show help for commands.'
-    ignore_require_venv = True
+def brown_tagset(tagpattern=None):
+    _format_tagset("brown_tagset", tagpattern)
 
-    def run(self, options, args):
-        from pip._internal.commands import commands_dict, get_similar_commands
 
-        try:
-            # 'pip help' with no args is handled by pip.__init__.parseopt()
-            cmd_name = args[0]  # the command we need help for
-        except IndexError:
-            return SUCCESS
+def claws5_tagset(tagpattern=None):
+    _format_tagset("claws5_tagset", tagpattern)
 
-        if cmd_name not in commands_dict:
-            guess = get_similar_commands(cmd_name)
 
-            msg = ['unknown command "%s"' % cmd_name]
-            if guess:
-                msg.append('maybe you meant "%s"' % guess)
+def upenn_tagset(tagpattern=None):
+    _format_tagset("upenn_tagset", tagpattern)
 
-            raise CommandError(' - '.join(msg))
 
-        command = commands_dict[cmd_name]()
-        command.parser.print_help()
+#####################################################################
+# UTILITIES
+#####################################################################
 
-        return SUCCESS
+
+def _print_entries(tags, tagdict):
+    for tag in tags:
+        entry = tagdict[tag]
+        defn = [tag + ": " + entry[0]]
+        examples = wrap(
+            entry[1], width=75, initial_indent="    ", subsequent_indent="    "
+        )
+        print("\n".join(defn + examples))
+
+
+def _format_tagset(tagset, tagpattern=None):
+    tagdict = load("help/tagsets/" + tagset + ".pickle")
+    if not tagpattern:
+        _print_entries(sorted(tagdict), tagdict)
+    elif tagpattern in tagdict:
+        _print_entries([tagpattern], tagdict)
+    else:
+        tagpattern = re.compile(tagpattern)
+        tags = [tag for tag in sorted(tagdict) if tagpattern.match(tag)]
+        if tags:
+            _print_entries(tags, tagdict)
+        else:
+            print("No matching tags found.")
+
+
+if __name__ == "__main__":
+    brown_tagset(r"NN.*")
+    upenn_tagset(r".*\$")
+    claws5_tagset("UNDEFINED")
+    brown_tagset(r"NN")
